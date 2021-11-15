a1c849f06bb6c43002811b41848e4393743b57a1
Sergey Khmelevskoy <xs290@me.com>
2021-11-15 18:44:14 +0300

    add sentry

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import '@styles/index.scss';
 import App from '@c/App';
 import { BrowserInfo, VersionCheck, History } from '@services';
+
+Sentry.init({
+  environment: 'production',
+  dsn: 'https://1a0ad3dd350f4718b38ddf68d5449b4b@o1068843.ingest.sentry.io/6063116',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
